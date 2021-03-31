9bf731b5a05a7acff7eca4e1e6daace0a4c332a8
sem-the-dev <semhar.tesfu@outlook.com>
2021-03-31 12:53:05 +0100

    attempt to add render function

--- client/static/js/index.js
+++ client/static/js/index.js
@@ -1,6 +1,15 @@
 const form = document.querySelector('form')
 form.addEventListener('submit', postEntry)
 
+window.addEventListener('hashchange', updateContent);
+
+
+function updateContent(){
+    const path = window.location.hash.substring(1).replace(/%20/g, " ");
+    console.log(getOnePost(path))
+    
+}
+
 
 
 
@@ -12,13 +21,14 @@ async function postEntry(e){
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(Object.fromEntries(new FormData(e.target)))
         }
-        console.log(new FormData(e.target))
         const r = await fetch(`http://localhost:3000/posts/`, options)
         const data = await r.json()
+        console.log(data)
     } catch (err) {
         console.warn(err);
     }
 }
+
 
 
 async function getOnePost(uniqueId){
@@ -26,7 +36,26 @@ async function getOnePost(uniqueId){
         const response = await fetch(`http://localhost:3000/posts/${uniqueId}`);
         const data = await response.json();
         return data;
+        renderFeed(data)
+        
     } catch (err) {
         console.warn(err);
     }
+}
+
+async function renderFeed(postData) {
+    const newPost = document.createElement('main');
+    const post = await getOnePost();
+    const renderPost = postData => {
+
+        const postSection = document.createElement('div');
+        const title = document.createElement('h3');
+        const body = document.createElement('p');
+        title.textContent = postData.title;
+        body.textContent = postData.body;
+        postSection.append(title);
+        postSection.append(body);
+    }
+    
+    newPost.append(postSection);
 }
