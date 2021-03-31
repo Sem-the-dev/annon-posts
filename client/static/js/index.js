b4048d3e0043ab27a2dc2afab4e1330f9bec0192
sem-the-dev <semhar.tesfu@outlook.com>
2021-03-31 17:19:24 +0100

    render posts

--- client/static/js/index.js
+++ client/static/js/index.js
@@ -6,13 +6,10 @@ window.addEventListener('hashchange', updateContent);
 
 function updateContent(){
     const path = window.location.hash.substring(1).replace(/%20/g, " ");
-    console.log(getOnePost(path))
-    
+    console.log(path)
+    getOnePost(path)
+    .then(d => renderPost(d))
 }
-
-
-
-
 async function postEntry(e){
     e.preventDefault();
     try {
@@ -23,39 +20,35 @@ async function postEntry(e){
         }
         const r = await fetch(`http://localhost:3000/posts/`, options)
         const data = await r.json()
-        console.log(data)
+        let url = window.location.href
+        let hash = '#'+data.unique_id.replace(/ /g,"%20")
+        window.location.href = url+hash
     } catch (err) {
         console.warn(err);
     }
 }
-
-
-
 async function getOnePost(uniqueId){
-    try {
-        const response = await fetch(`http://localhost:3000/posts/${uniqueId}`);
-        const data = await response.json();
-        return data;
-        renderFeed(data)
-        
-    } catch (err) {
-        console.warn(err);
-    }
+    return new Promise(async (res, rej) => {
+        try {
+            const response = await fetch(`http://localhost:3000/posts/${uniqueId}`);
+            const data = await response.json();
+            res(data);
+        } catch (err) {
+            rej(`Error retrieving posts: ${err}`)
+        }
+    })
 }
-
-async function renderFeed(postData) {
-    const newPost = document.createElement('main');
-    const post = await getOnePost();
-    const renderPost = postData => {
-
-        const postSection = document.createElement('div');
-        const title = document.createElement('h3');
-        const body = document.createElement('p');
-        title.textContent = postData.title;
-        body.textContent = postData.body;
-        postSection.append(title);
-        postSection.append(body);
-    }
-    
-    newPost.append(postSection);
+function renderPost(postData) {
+    form.innerHTML="";
+    const postSection = document.createElement('div');
+    const title = document.createElement('h2');
+    const username = document.createElement('h4');
+    const body = document.createElement('p');
+    title.textContent = postData.title;
+    username.textContent = postData.username;
+    body.textContent = postData.body;
+    postSection.append(title);
+    postSection.append(username);
+    postSection.append(body);
+    form.append(postSection);
 }
